f038a157f33b8a126c32f44753276f68fea989fc
aimeeallen94 <>
2023-06-24 19:03:45 +0000

    beginning functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,40 @@
 function runGame();
+
+function mySelection();
+let myChoice = document.getElementById('myChoice');
+let choices = document.getElementsByTagName('button');
+let myPick;
+
+choices.forEach(choice => choices.addEventListener(click, (event) => {
+    myPick = event.target.id;
+    myChoice.innerHTML = myPick;
+}));
+
+function computerSelection();
+
+/* code for generating random number for computer selection */
+let randomNumber = Math.floor(Math.random() * 6 + 1);
+ if (randomNumber === 1) {
+    computerChoice = 'rock'
+} if (randomNumber === 2) {
+    computerChoice = 'paper';
+} if (randomNumber === 3) {
+    computerChoice = 'scissors';
+} if (randomNumber === 4) {
+    computerChoice = 'lizard';
+} if (randomNumber === 5) {
+    computerChoice = 'spock';
+} 
+
+
 function compareAnswers();
+
+
 function incrementMyScore();
+
+
 function incrementComputerScore();
+
+
 function incrementAttempts();
+
