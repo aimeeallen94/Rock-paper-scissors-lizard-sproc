3302dc85f755a54002c20b58ed605450a812c90f
aimeeallen94 <>
2023-06-25 11:23:25 +0000

    getting my choice

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,40 +1,42 @@
-function runGame();
-
-function mySelection();
-let myChoice = document.getElementById('myChoice');
-let choices = document.getElementsByTagName('button');
+let myChoice = document.getElementById('my-choice');
+let choices = Array.from(document.getElementsByTagName('button'));
 let myPick;
 
-choices.forEach(choice => choices.addEventListener(click, (event) => {
+choices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event) => {
     myPick = event.target.id;
     myChoice.innerHTML = myPick;
 }));
 
-function computerSelection();
+function runGame() { };
 
-/* code for generating random number for computer selection */
-let randomNumber = Math.floor(Math.random() * 6 + 1);
- if (randomNumber === 1) {
-    computerChoice = 'rock'
-} if (randomNumber === 2) {
-    computerChoice = 'paper';
-} if (randomNumber === 3) {
-    computerChoice = 'scissors';
-} if (randomNumber === 4) {
-    computerChoice = 'lizard';
-} if (randomNumber === 5) {
-    computerChoice = 'spock';
-} 
+function computerSelection() {
+
+    let computerChoice = document.getElementById('computer-choice'); {
+
+        /* code for generating random number for computer selection */
+        let randomNumber = Math.floor(Math.random() * 6 + 1);
+        if (randomNumber === 1) {
+            computerChoice = 'rock';
+        } if (randomNumber === 2) {
+            computerChoice = 'paper';
+        } if (randomNumber === 3) {
+            computerChoice = 'scissors';
+        } if (randomNumber === 4) {
+            computerChoice = 'lizard';
+        } if (randomNumber === 5) {
+            computerChoice = 'spock';
+        }
+    }
+}
+
+function compareAnswers() { };
 
 
-function compareAnswers();
+function incrementMyScore() { };
 
 
-function incrementMyScore();
+function incrementComputerScore() { };
 
 
-function incrementComputerScore();
-
-
-function incrementAttempts();
+function incrementAttempts() { };
 
